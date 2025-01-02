2b24d3f844cb9a2a9576da4314f58e80356db25d
tanveeeer00 <shaikhtanveer5205@gmail.com>
2025-01-03 03:02:45 +0530

    subscription feature

--- bot.js
+++ bot.js
@@ -8,17 +8,20 @@ const bot = new TelegramBot(process.env.BOT_TOKEN, { polling: true });
 const adminChatId = process.env.ADMIN_CHAT_ID;
 console.log("Admin chat ID:", adminChatId);
 
+const channelChatId = "@Demon_Slayer_season_1_2_3_4_anim";
+
 const authorizedUser = (msg) => {
   const userId = msg.from.id;
   if (!adminChatId.includes(userId)) {
-    bot.sendMessage(chatId, "You are not authorized to upload files.");
+    bot.sendMessage(msg.chat.id, "You are not authorized to upload files.");
     return;
   }
 };
 
 const handleFileUpload = (msg, fileId, fileName = "File") => {
   const chatId = msg.chat.id;
-  authorizedUser(msg);
+  // Check if the user is authorized
+  if (authorizedUser(msg)) return;
   console.log(msg);
 
   const uniqueId = uuidv4();
@@ -41,9 +44,10 @@ const handleFileUpload = (msg, fileId, fileName = "File") => {
 };
 
 bot.on("message", (msg) => {
-  const chatId = msg.chat.id;
-  authorizedUser(msg);
-  bot.sendMessage(chatId, "Received your message");
+  //   const chatId = msg.chat.id;
+  //   if (authorizedUser(msg)) {
+  //     bot.sendMessage(chatId, "Received your message");
+  //   }
 });
 
 // Handle documents
@@ -63,57 +67,141 @@ bot.on("photo", (msg) => {
   handleFileUpload(msg, fileId, "Image");
 });
 
+bot.on("polling_error", (error) => {
+  console.error("Polling Error:", error);
+});
+
 bot.onText(/\/getfile_(.+)/, (msg, match) => {
   const chatId = msg.chat.id;
   const uniqueId = match[1];
+  bot
+    .getChatMember(channelChatId, msg.from.id)
+    .then((data) => {
+      console.log("User data:", data);
+      if (
+        data.status === "member" ||
+        data.status === "administrator" ||
+        data.status === "creator"
+      ) {
+        console.log("User is a member of the channel");
+        db.get(
+          `SELECT file_id, file_name FROM files WHERE id = ?`,
+          [uniqueId],
+          (err, row) => {
+            if (err) {
+              console.error("Database error:", err);
+              bot.sendMessage(
+                chatId,
+                "An error occurred while retrieving the file. Please try again."
+              );
+              return;
+            }
 
-  // Query the database for the file
-  db.get(
-    `SELECT file_id, file_name FROM files WHERE id = ?`,
-    [uniqueId],
-    (err, row) => {
-      if (err) {
-        console.error("Database error:", err);
+            if (!row) {
+              bot.sendMessage(chatId, "Invalid or expired link.");
+              return;
+            }
+
+            const { file_id, file_name } = row;
+
+            // Determine the file type based on its metadata
+            if (file_name === "Image") {
+              // Handle photos
+              bot
+                .sendPhoto(chatId, file_id, { protect_content: true })
+                .catch((err) => {
+                  console.error("Error sending photo:", err);
+                  bot.sendMessage(chatId, "Failed to send the photo.");
+                });
+            } else if (file_name === "Video") {
+              // Handle videos
+              bot
+                .sendVideo(chatId, file_id, { protect_content: true })
+                .catch((err) => {
+                  console.error("Error sending video:", err);
+                  bot.sendMessage(chatId, "Failed to send the video.");
+                });
+            } else {
+              // Handle documents or other files
+              bot
+                .sendDocument(chatId, file_id, { protect_content: true })
+                .catch((err) => {
+                  console.error("Error sending document:", err);
+                  bot.sendMessage(chatId, "Failed to send the document.");
+                });
+            }
+          }
+        );
+      } else {
         bot.sendMessage(
           chatId,
-          "An error occurred while retrieving the file. Please try again."
+          "You need to join the channel to access the file. Please join the channel and try again. channel link: https://t.me/Demon_Slayer_season_1_2_3_4_anim"
         );
-        return;
       }
-
-      if (!row) {
-        bot.sendMessage(chatId, "Invalid or expired link.");
-        return;
-      }
-
-      const { file_id, file_name } = row;
-
-      // Determine the file type based on its metadata
-      if (file_name === "Image") {
-        // Handle photos
-        bot
-          .sendPhoto(chatId, file_id, { protect_content: true })
-          .catch((err) => {
-            console.error("Error sending photo:", err);
-            bot.sendMessage(chatId, "Failed to send the photo.");
-          });
-      } else if (file_name === "Video") {
-        // Handle videos
-        bot
-          .sendVideo(chatId, file_id, { protect_content: true })
-          .catch((err) => {
-            console.error("Error sending video:", err);
-            bot.sendMessage(chatId, "Failed to send the video.");
-          });
-      } else {
-        // Handle documents or other files
-        bot
-          .sendDocument(chatId, file_id, { protect_content: true })
-          .catch((err) => {
-            console.error("Error sending document:", err);
-            bot.sendMessage(chatId, "Failed to send the document.");
-          });
-      }
-    }
-  );
+    })
+    .catch((err) => {
+      console.error("Error checking user membership:", err);
+      bot.sendMessage(
+        chatId,
+        "An error occurred while checking your membership. Please try again."
+      );
+    });
+  // Query the database for the file
 });
+
+// bot.onText(/\/getfile_(.+)/, (msg, match) => {
+//   const chatId = msg.chat.id;
+//   const uniqueId = match[1];
+
+//   console.log("User is a member of the channel");
+//   db.get(
+//     `SELECT file_id, file_name FROM files WHERE id = ?`,
+//     [uniqueId],
+//     (err, row) => {
+//       if (err) {
+//         console.error("Database error:", err);
+//         bot.sendMessage(
+//           chatId,
+//           "An error occurred while retrieving the file. Please try again."
+//         );
+//         return;
+//       }
+
+//       if (!row) {
+//         bot.sendMessage(chatId, "Invalid or expired link.");
+//         return;
+//       }
+
+//       const { file_id, file_name } = row;
+
+//       // Determine the file type based on its metadata
+//       if (file_name === "Image") {
+//         // Handle photos
+//         bot
+//           .sendPhoto(chatId, file_id, { protect_content: true })
+//           .catch((err) => {
+//             console.error("Error sending photo:", err);
+//             bot.sendMessage(chatId, "Failed to send the photo.");
+//           });
+//       } else if (file_name === "Video") {
+//         // Handle videos
+//         bot
+//           .sendVideo(chatId, file_id, { protect_content: true })
+//           .catch((err) => {
+//             console.error("Error sending video:", err);
+//             bot.sendMessage(chatId, "Failed to send the video.");
+//           });
+//       } else {
+//         // Handle documents or other files
+//         bot
+//           .sendDocument(chatId, file_id, { protect_content: true })
+//           .catch((err) => {
+//             console.error("Error sending document:", err);
+//             bot.sendMessage(chatId, "Failed to send the document.");
+//           });
+//       }
+//     }
+//   );
+
+//   // Query the database for the file
+// });
