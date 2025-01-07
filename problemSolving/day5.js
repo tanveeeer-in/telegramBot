4a077ef5c9422c5f99a3c3eca6b73139f45df6aa
tanveeeer00 <shaikhtanveer5205@gmail.com>
2025-01-07 22:05:53 +0530

    day 5

---
+++ problemSolving/day5.js
@@ -0,0 +1,13 @@
+function selectionSorting(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[i] > arr[j]) {
+        let temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+      }
+    }
+  }
+  return arr;
+}
+console.log(selectionSorting([64, 34, 25, 12, 22, 11, 90]));
