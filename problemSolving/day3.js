a5e8bd8ad102f6b4116f8853b550e11094a9d061
tanveeeer00 <shaikhtanveer5205@gmail.com>
2025-01-07 21:16:38 +0530

    day 3

---
+++ problemSolving/day3.js
@@ -0,0 +1,14 @@
+function findDuplication(arr) {
+  let duplicate = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[i] == arr[j]) {
+        duplicate.push(arr[i]);
+      }
+    }
+  }
+
+  return duplicate;
+}
+console.log(findDuplication([4, 3, 2, 7, 8, 2, 3,2 ,2,1]));
