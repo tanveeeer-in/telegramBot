34388e8d0d276c5ddd86608a45a28ac19ea3b107
Tanveer <shaikhtanveer5205@gmail.com>
2025-02-04 23:40:14 +0530

    Update day6.js

--- problemSolving/day6.js
+++ problemSolving/day6.js
@@ -1,5 +1,5 @@
 function insertionSorting() {
-  let arr = [29, 10, 14, 37, 13,14,3];
+  let arr = [29, 10,12, 14, 37, 13,14,3];
 
   for (let i = 1; i < arr.length; i++) {
     for (let j = i; j > 0; j--) {
